393b264e83fa1ed9c4eaf94a6243becabea3bf3c
Valiev Ali <avaliev024@gmail.com>
2023-08-04 21:32:35 +0300

    added errormessages

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 const Search = () => {
   const navigate = useNavigate();
   const [query, setQuery] = useState("");
+  const [message, setMessage] = useState();
   const apiKey = "f3f79b864b0c4957c6f6eb1b595af2b1";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=metric`;
 
@@ -12,19 +13,19 @@ const Search = () => {
     e.preventDefault();
     const response = await fetch(url);
     const weatherData = await response.json();
-    localStorage.setItem("weatherData", JSON.stringify(weatherData));
-    navigate("/results");
+    if (weatherData.cod === 200) {
+      localStorage.setItem("weatherData", JSON.stringify(weatherData));
+      navigate("/results");
+    } else {
+      setMessage(weatherData.message);
+    }
   };
 
   return (
     <div className="searchContainer">
       <form className="searchForm" onSubmit={fetchData}>
         <div className="centerContainer">
-          <img
-            src="../images/logo.webp"
-            alt="logo"
-            className="logo"
-          />
+          <img src="../images/logo.webp" alt="logo" className="logo" />
           <input
             className="searchInput"
             type="text"
@@ -32,6 +33,7 @@ const Search = () => {
             value={query}
             onChange={(e) => setQuery(e.target.value)}
           />
+          {message && <p className="message">{message}</p>}
         </div>
       </form>
     </div>
